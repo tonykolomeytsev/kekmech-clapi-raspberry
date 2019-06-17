588defbbfe1bcb23395268e8bd230c436501de8d
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-06-17 06:44:02 +0300

    Fast bugfix

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
 	author_email="tonykolomeytsev@gmail.com",
 	url="https://github.com/tonykolomeytsev/kekmech-clapi-raspberry",
 	py_modules=['clapi', 'asynclapi'],
-    libraries=['pyserial'],
 	license='MIT License',
     classifiers=[
         "Programming Language :: Python :: 3",
